5f7def678c9d7450a67de3f53f33602ea002dc0b
dcsan <dc@rikai.co>
2018-08-11 10:32:57 +0800

    deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,22 +4,25 @@
 set -e
 
 # build
-npm run docs:build
+yarn docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+rm -rf docs
+mv src/.vuepress/dist docs
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
+git push
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# git push -f git@github.com:crowdlingo/course.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# cd -
